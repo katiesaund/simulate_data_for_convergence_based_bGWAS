cefe81fc5b80fee2070bc63b0e1b52a79126fed5
Katie Saund <katiephd@umich.edu>
2020-01-07 14:28:38 -0500

    Copy two functions from hogwash

---
+++ R/transition_edges.R
@@ -0,0 +1,118 @@
+#' prep_geno_trans_for_phyc
+#'
+#' @description Discrete testing requires two different definitions of genotype
+#'  transition, one for PhyC and one for Synchronous Test. This function
+#'  converts geno_trans$transition from the object created for synchronous test
+#'  to the version required for the PhyC test.
+#'
+#'  @details For PhyC prepare genotype transition as below: keep only WT ->
+#'   mutant transitions (0 -> 1)
+#'
+#' @param geno Matrix. Nrow = Ntip(Tree). Ncol = number of genotypes. Binary.
+#' @param genotype_transition List of lists. Each sublist has two vectors.
+#'  * $transition. Length  == Nedge(tree). 0/1
+#'  * $trans_dir. -1/0/1. Length == Nedge(tree).
+#'
+#' @return genotype_transition. List with $transition and $trans_dir.
+prep_geno_trans_for_phyc <- function(geno, genotype_transition){
+  # Check inputs ---------------------------------------------------------------
+  check_if_binary_matrix(geno)
+  check_equal(length(genotype_transition), ncol(geno))
+  check_if_binary_vector(genotype_transition[[1]]$transition)
+
+  # Function -------------------------------------------------------------------
+  for (k in 1:ncol(geno)) {
+    parent_WT_child_mutant <- 1 # 1 implies parent < child
+    parent_mutant_child_WT <- -1 # -1 implies parent > child
+    no_transition <- 0 # 0 implies parent == child
+    genotype_transition[[k]]$transition <-
+      as.numeric(genotype_transition[[k]]$trans_dir == parent_WT_child_mutant)
+    genotype_transition[[k]]$trans_dir[genotype_transition[[k]]$trans_dir
+                                       == parent_mutant_child_WT] <-
+      no_transition # erase transitions in the opposite direction
+  }
+
+  # Return output --------------------------------------------------------------
+  return(genotype_transition)
+} # end prep_geno_trans_for_phyc()
+
+
+#' identify_transition_edges
+#'
+#' @description Given a reconstruction identify which edges on tree are
+#'  transitions. A transition edge is one in which the parent node and child
+#'  node differ.
+#'
+#' @param tr Phylo.
+#' @param mat Matrix. Phenotype (either continuous or binary) or a binary
+#'   genotype. Dim: nrow = Ntrip(tr) x ncol = {1 if phenotype or number of
+#'   genotypes}.
+#' @param num Integer. Index of current genotype (column number in genotype matrix).
+#' @param node_recon Numeric vector. Either pheno_recon_and_conf$node_anc_rec or
+#'  geno_recon_and_conf[[k]]$node_anc_rec. Length = Nnode(tr). Ancestral
+#'  reconstruction value for each node.
+#' @param disc_cont Character string. Either "discrete" or "continuous".
+#'
+#' @return List.
+#'  * $transition: Continuous: NA, because this is meaningless for continuous data
+#'    as all edges will be transitions. Discrete: Numeric vector of 0 or 1. 0
+#'    indicates parent and child node are identical. 1 indicates parent and
+#'    child node differ.
+#'  * $trans_dir: Numeric Vector. Continuous and discrete: gives the direction
+#'    of the transition. When parent < child or parent_0_child_1 value is +1.
+#'    When parent > child or parent_1_child_0 value is -1.
+#' @noRd
+identify_transition_edges <- function(tr, mat, num, node_recon, disc_cont){
+  # Check input ----------------------------------------------------------------
+  check_for_root_and_bootstrap(tr)
+  check_tree_is_valid(tr)
+  check_is_number(num)
+  check_str_is_discrete_or_continuous(disc_cont)
+
+  # FUNCTION -------------------------------------------------------------------
+  transition <- transition_direction <-
+    parent_node <- child_node <- integer(ape::Nedge(tr))
+  older <- 1 # older node is 1st column in tr$edge
+  younger <- 2 # younger node is 2nd column in tr$edge
+  parent_0_child_1 <- 1
+  parent_1_child_0 <- -1
+  parent_equals_child <- 0
+  both_parent_and_child_are_one <- 2
+
+
+  for (i in 1:ape::Nedge(tr)) {
+    if (is_tip(tr$edge[i, older], tr)) {
+      stop("tree invalid")
+    }
+    parent_node[i] <- node_recon[tr$edge[i, older] - ape::Ntip(tr)]
+    if (is_tip(tr$edge[i, younger], tr)) {
+      # child is a tip
+      child_node[i]  <- mat[, num][tr$edge[i, younger]]
+    } else {
+      # child is internal nodes
+      child_node[i]  <- node_recon[tr$edge[i, younger] - ape::Ntip(tr)]
+    }
+
+    transition[i] <- sum(parent_node[i] + child_node[i])
+    # transition[i] is either 0, 1, or 2 for discrete traits
+    # transition[i] is not to be used when the trait is continuous because all,
+    # or very nearly all edges are transition edges.
+
+    if (parent_node[i] > child_node[i]) {
+      transition_direction[i] <- parent_1_child_0
+    } else if (parent_node[i] < child_node[i]) {
+      transition_direction[i] <- parent_0_child_1
+    }
+
+    if (disc_cont == "discrete") {
+      transition[transition == both_parent_and_child_are_one] <-
+        parent_equals_child # parent_node == child_node, then no transition.
+    } else {
+      transition <- NA # Not used when the trait is continuous.
+    }
+  }
+  # Check and return output ----------------------------------------------------
+  results <- list("transition" = transition, "trans_dir" = transition_direction)
+  return(results)
+} # end identify_transition_edges()
+

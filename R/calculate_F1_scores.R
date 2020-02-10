b2846568b6ca3e461eb2b695e3300cd1c6ecfad6
Katie Saund <katiephd@umich.edu>
2020-02-10 14:16:35 -0500

    Comment out continuous stuff

--- R/calculate_F1_scores.R
+++ R/calculate_F1_scores.R
@@ -164,96 +164,97 @@ for (h in 1:length(pheno_index)) {
   }
 }
 
-# CONTINUOUS
+# CONTINUOUS ----
 # for (h in 1:length(pheno_index)) {
-  for (j in 1:length(data_types)) {
-    for (k in 1:length(tree_index)) {
-      current_file_name <- paste0(data_dir, 
-                                  "hogwash_continuous_continuous_pheno_",
-                                  data_types[j],
-                                  "_tree_", 
-                                  tree_index[k],
-                                  "_pheno_", 
-                                  pheno_index[h],
-                                  ".rda")
-      current_key_name <- paste0(data_dir,
-                                 "key_continuous_geno_continuous_pheno_",
-                                 data_types[j], 
-                                 "_tree_", 
-                                 tree_index[k],
-                                 "_pheno_", 
-                                 pheno_index[h],
-                                 ".tsv")
-      
-      if (file.exists(current_file_name) & file.exists(current_key_name)) {
-        hogwash_results <- local(get(load(current_file_name)))
-        pvals <- rownames_to_column(hogwash_results$hit_pvals, var = "genotype") 
-        key <- read.table(current_key_name, header = 1, stringsAsFactors = FALSE)
-        
-        pvals_and_key <- left_join(pvals, key, by = "genotype")
-        
-        true_positive <- pvals_and_key %>% filter(association_with_phenotype == "associated" & fdr_corrected_pvals > alpha) %>% nrow()
-        true_negative <- pvals_and_key %>% filter(association_with_phenotype == "null" & fdr_corrected_pvals < alpha) %>% nrow()
-        false_positive <- pvals_and_key %>% filter(association_with_phenotype == "null" & fdr_corrected_pvals > alpha) %>% nrow()
-        false_negative <- pvals_and_key %>% filter(association_with_phenotype == "associated" & fdr_corrected_pvals < alpha) %>% nrow()
-        
-        precision <- true_positive / sum(true_positive, false_positive)
-        recall <- true_positive / sum(false_negative, true_positive)
-        false_positive_rate <- false_positive / sum(false_positive, true_negative)
-        positive_predictive_value <- true_positive / sum(true_positive, false_positive)
-        
-        f1_score <- (2 * precision * recall) / (precision + recall)
-        
-        hogwash_results <- local(get(load(current_file_name)))
-        gamma_value <- hogwash_results$gamma$gamma_avg
-        # calculate delta epsilon: 
-        null_epsilon_dist <- hogwash_results$gamma$epsilon[key$association_with_phenotype == "null"]
-        assoc_epsilon_dist <- hogwash_results$gamma$epsilon[key$association_with_phenotype == "associated"]
-        delta_epsilon <- mean(assoc_epsilon_dist) - mean(null_epsilon_dist)
-        beta_pheno <- hogwash_results$gamma$pheno_beta
-        mean_beta_geno <- mean(hogwash_results$gamma$geno_beta)
-        
-        pheno_presence <- pheno_absence <- NULL 
-        for (p in 1:length(hogwash_results$contingency_table)) {
-          pheno_presence <- c(pheno_presence, sum(hogwash_results$contingency_table[[p]][, 1]))
-          pheno_absence <- c(pheno_absence, sum(hogwash_results$contingency_table[[p]][, 2]))
-        }
-        avg_pheno_state_balance <- mean(pheno_presence) / sum(mean(pheno_presence), mean(pheno_absence))
-        
-        
-        hogwash_results <- NULL
-        
-        current_cont_row <- current_cont_row + 1
-        continuous_f1_mat[current_cont_row, 2] <- as.character(data_types[j])
-        continuous_f1_mat[current_cont_row, 3] <- tree_index[k]
-        continuous_f1_mat[current_cont_row, 4] <- "continuous"
-        continuous_f1_mat[current_cont_row, 5] <- f1_score
-        continuous_f1_mat[current_cont_row, 6] <- true_positive
-        continuous_f1_mat[current_cont_row, 7] <- true_negative
-        continuous_f1_mat[current_cont_row, 8] <- false_positive
-        continuous_f1_mat[current_cont_row, 9] <- false_negative
-        continuous_f1_mat[current_cont_row, 10] <- current_num_tip <- NA
-        continuous_f1_mat[current_cont_row, 11] <- current_num_geno <- NA
-        continuous_f1_mat[current_cont_row, 12] <- gamma_value
-        continuous_f1_mat[current_cont_row, 13] <- recall
-        continuous_f1_mat[current_cont_row, 14] <- precision
-        continuous_f1_mat[current_cont_row, 15] <- false_positive_rate
-        continuous_f1_mat[current_cont_row, 16] <- positive_predictive_value
-        continuous_f1_mat[current_cont_row, 17] <- pheno_index[h]
-        continuous_f1_mat[current_cont_row, 18] <- delta_epsilon
-        continuous_f1_mat[current_cont_row, 19] <- beta_pheno
-        continuous_f1_mat[current_cont_row, 20] <- mean_beta_geno
-        continuous_f1_mat[current_cont_row, 21] <- avg_pheno_state_balance
-        
-      } else {
-        print("file does not exist:")
-        print(current_file_name)
-      }
-        
-    }
-  }
-}
-      
+#   for (j in 1:length(data_types)) {
+#     for (k in 1:length(tree_index)) {
+#       current_file_name <- paste0(data_dir, 
+#                                   "hogwash_continuous_continuous_pheno_",
+#                                   data_types[j],
+#                                   "_tree_", 
+#                                   tree_index[k],
+#                                   "_pheno_", 
+#                                   pheno_index[h],
+#                                   ".rda")
+#       current_key_name <- paste0(data_dir,
+#                                  "key_continuous_geno_continuous_pheno_",
+#                                  data_types[j], 
+#                                  "_tree_", 
+#                                  tree_index[k],
+#                                  "_pheno_", 
+#                                  pheno_index[h],
+#                                  ".tsv")
+#       
+#       if (file.exists(current_file_name) & file.exists(current_key_name)) {
+#         hogwash_results <- local(get(load(current_file_name)))
+#         pvals <- rownames_to_column(hogwash_results$hit_pvals, var = "genotype") 
+#         key <- read.table(current_key_name, header = 1, stringsAsFactors = FALSE)
+#         
+#         pvals_and_key <- left_join(pvals, key, by = "genotype")
+#         
+#         true_positive <- pvals_and_key %>% filter(association_with_phenotype == "associated" & fdr_corrected_pvals > alpha) %>% nrow()
+#         true_negative <- pvals_and_key %>% filter(association_with_phenotype == "null" & fdr_corrected_pvals < alpha) %>% nrow()
+#         false_positive <- pvals_and_key %>% filter(association_with_phenotype == "null" & fdr_corrected_pvals > alpha) %>% nrow()
+#         false_negative <- pvals_and_key %>% filter(association_with_phenotype == "associated" & fdr_corrected_pvals < alpha) %>% nrow()
+#         
+#         precision <- true_positive / sum(true_positive, false_positive)
+#         recall <- true_positive / sum(false_negative, true_positive)
+#         false_positive_rate <- false_positive / sum(false_positive, true_negative)
+#         positive_predictive_value <- true_positive / sum(true_positive, false_positive)
+#         
+#         f1_score <- (2 * precision * recall) / (precision + recall)
+#         
+#         hogwash_results <- local(get(load(current_file_name)))
+#         gamma_value <- hogwash_results$gamma$gamma_avg
+#         # calculate delta epsilon: 
+#         null_epsilon_dist <- hogwash_results$gamma$epsilon[key$association_with_phenotype == "null"]
+#         assoc_epsilon_dist <- hogwash_results$gamma$epsilon[key$association_with_phenotype == "associated"]
+#         delta_epsilon <- mean(assoc_epsilon_dist) - mean(null_epsilon_dist)
+#         beta_pheno <- hogwash_results$gamma$pheno_beta
+#         mean_beta_geno <- mean(hogwash_results$gamma$geno_beta)
+#         
+#         pheno_presence <- pheno_absence <- NULL 
+#         for (p in 1:length(hogwash_results$contingency_table)) {
+#           pheno_presence <- c(pheno_presence, sum(hogwash_results$contingency_table[[p]][, 1]))
+#           pheno_absence <- c(pheno_absence, sum(hogwash_results$contingency_table[[p]][, 2]))
+#         }
+#         avg_pheno_state_balance <- mean(pheno_presence) / sum(mean(pheno_presence), mean(pheno_absence))
+#         
+#         
+#         hogwash_results <- NULL
+#         
+#         current_cont_row <- current_cont_row + 1
+#         continuous_f1_mat[current_cont_row, 2] <- as.character(data_types[j])
+#         continuous_f1_mat[current_cont_row, 3] <- tree_index[k]
+#         continuous_f1_mat[current_cont_row, 4] <- "continuous"
+#         continuous_f1_mat[current_cont_row, 5] <- f1_score
+#         continuous_f1_mat[current_cont_row, 6] <- true_positive
+#         continuous_f1_mat[current_cont_row, 7] <- true_negative
+#         continuous_f1_mat[current_cont_row, 8] <- false_positive
+#         continuous_f1_mat[current_cont_row, 9] <- false_negative
+#         continuous_f1_mat[current_cont_row, 10] <- current_num_tip <- NA
+#         continuous_f1_mat[current_cont_row, 11] <- current_num_geno <- NA
+#         continuous_f1_mat[current_cont_row, 12] <- gamma_value
+#         continuous_f1_mat[current_cont_row, 13] <- recall
+#         continuous_f1_mat[current_cont_row, 14] <- precision
+#         continuous_f1_mat[current_cont_row, 15] <- false_positive_rate
+#         continuous_f1_mat[current_cont_row, 16] <- positive_predictive_value
+#         continuous_f1_mat[current_cont_row, 17] <- pheno_index[h]
+#         continuous_f1_mat[current_cont_row, 18] <- delta_epsilon
+#         continuous_f1_mat[current_cont_row, 19] <- beta_pheno
+#         continuous_f1_mat[current_cont_row, 20] <- mean_beta_geno
+#         continuous_f1_mat[current_cont_row, 21] <- avg_pheno_state_balance
+#         
+#       } else {
+#         print("file does not exist:")
+#         print(current_file_name)
+#       }
+#         
+#     }
+#   }
+# }
+
+# Summarize output ----      
 # f1_mat <- rbind(continuous_f1_mat, discrete_f1_mat)
 f1_mat <- discrete_f1_mat
 f1_mat[f1_mat[, 4] == "synchronous", 4] <- "sync"

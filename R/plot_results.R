7e1de4b5753161022f98a50d75fa7a6c5ddc95c2
Katie Saund <katiephd@umich.edu>
2020-02-14 16:53:30 -0500

    Add new plots and update some plots to have lines of best fit

--- R/plot_results.R
+++ R/plot_results.R
@@ -62,7 +62,7 @@ df %>%
                        y = F1_score)) +  
   geom_smooth(aes(linetype = test, color = phenotype_phylogenetic_signal), se = FALSE) + 
   theme_bw() + 
-  labs(title = "F1 score vs Epsilon (Loess line of best fit") + 
+  labs(title = "F1 score vs Epsilon (Loess line of best fit)") + 
   xlab("Epsilon Threshold") + 
   ylab("F1 Score")
 ggsave("../figures/F1_score_vs_epsilon_line_plot.pdf", height = 6, width = 8.5, units = "in")
@@ -83,6 +83,22 @@ df %>%
   facet_grid(alpha_threshold ~ epsilon_threshold, labeller = labeller(alpha_threshold = alpha_label, epsilon_threshold = epsilon_label))
 ggsave("../figures/PPV_vs_test_alpha_vs_epsilon.pdf", height = 6, width = 8.5, units = "in")
 
+# Save as line plots
+df %>% 
+  filter(alpha_threshold < 3) %>% 
+  select(positive_predictive_value, phenotype_phylogenetic_signal, tree_id, test, phenotype_id, alpha_threshold, epsilon_threshold) %>%
+  unique() %>% 
+  filter(!is.na(positive_predictive_value)) %>% 
+  ggplot(mapping = aes(x = epsilon_threshold,
+                       y = positive_predictive_value)) +  
+  geom_smooth(aes(linetype = test, color = phenotype_phylogenetic_signal), se = FALSE) + 
+  theme_bw() + 
+  labs(title = "PPV vs Epsilon (Loess line of best fit)") + 
+  xlab("Epsilon Threshold") + 
+  ylab("Positive Predictive Value")
+ggsave("../figures/PPV_vs_epsilon_line_plot.pdf", height = 6, width = 8.5, units = "in")
+
+
 df %>% 
   select(test, phenotype_phylogenetic_signal, phenotype_id, tree_id, phenotype_state_balance) %>% 
   unique() %>% 
@@ -93,3 +109,77 @@ df %>%
   xlab("Phenotype state balance") + 
   ylab("Count")
 ggsave("../figures/Phenotype_state_balance_histrogram.pdf", height = 6, width = 8.5, units = "in")
+
+
+# false_positive_rate
+# Save as line plots
+df %>% 
+  filter(alpha_threshold < 3) %>% 
+  select(false_positive_rate, phenotype_phylogenetic_signal, tree_id, test, phenotype_id, alpha_threshold, epsilon_threshold) %>%
+  unique() %>% 
+  filter(!is.na(false_positive_rate)) %>% 
+  ggplot(mapping = aes(x = epsilon_threshold,
+                       y = false_positive_rate)) +  
+  geom_smooth(aes(linetype = test, color = phenotype_phylogenetic_signal), se = FALSE) + 
+  theme_bw() + 
+  labs(title = "FPR vs Epsilon (Loess line of best fit)") + 
+  xlab("Epsilon Threshold") + 
+  ylab("False Positive Rate")
+ggsave("../figures/FPR_vs_epsilon_line_plot.pdf", height = 6, width = 8.5, units = "in")
+
+
+
+# false_negative_rate
+# Save as line plots
+df %>% 
+  mutate(false_negative_rate = false_negative / sum(false_negative, true_positive)) %>% 
+  filter(alpha_threshold < 3) %>% 
+  filter(!is.na(false_negative_rate)) %>% 
+  select(false_negative_rate, phenotype_phylogenetic_signal, tree_id, test, phenotype_id, epsilon_threshold) %>%
+  unique() %>% 
+  ggplot(mapping = aes(x = epsilon_threshold,
+                       y = false_negative_rate)) +  
+  geom_smooth(aes(linetype = test, color = phenotype_phylogenetic_signal), se = FALSE) + 
+  theme_bw() + 
+  labs(title = "FNR vs Epsilon (Loess line of best fit)") + 
+  xlab("Epsilon Threshold") + 
+  ylab("False Negative Rate")
+ggsave("../figures/FNR_vs_epsilon_line_plot.pdf", height = 6, width = 8.5, units = "in")
+
+# observed gamma_value
+# Save as line plots
+df %>% 
+  filter(alpha_threshold < 3) %>% 
+  select(observed_gamma_value, epsilon, phenotype_phylogenetic_signal, test) %>%
+  filter(!is.na(observed_gamma_value)) %>% 
+  group_by(phenotype_phylogenetic_signal, test) %>% 
+  mutate(g_median = median(observed_gamma_value), e_median = median(epsilon)) %>% 
+  ggplot(mapping = aes(x = g_median, y = e_median, shape = phenotype_phylogenetic_signal, col = test)) +  
+  geom_point() + 
+  theme_bw() + 
+  labs(title = "Median Gamma vs Median Epsilon by group") + 
+  xlab("Median Epsilon") + 
+  ylab("Median Gamma")
+ggsave("../figures/Median_gamma_vs_median_epsilon_dot_plot.pdf", height = 6, width = 8.5, units = "in")
+
+
+# Save as line plots
+df %>% 
+  filter(alpha_threshold < 3) %>% 
+  mutate(tp_over_all = 100 * true_positive / sum(true_negative, true_negative, false_positive, false_negative), 
+         tn_over_all = 100 * true_negative / sum(true_negative, true_negative, false_positive, false_negative),
+         fp_over_all = 100 * false_positive / sum(true_negative, true_negative, false_positive, false_negative),
+         fn_over_all = 100 * false_negative / sum(true_negative, true_negative, false_positive, false_negative)) %>% 
+  select(epsilon_threshold, test, phenotype_phylogenetic_signal, tp_over_all, tn_over_all, fp_over_all, fn_over_all) %>% 
+  reshape2::melt(id.vars = c("epsilon_threshold", "test", "phenotype_phylogenetic_signal"), 
+                 variable.name = "metric", 
+                 value.name = "percent") %>% 
+  ggplot(aes(x = epsilon_threshold, y = percent, col = metric)) +  
+  geom_smooth() + 
+  theme_bw() + 
+  labs(title = "TP/TN/FP/FN vs epsilon threshold by group") + 
+  xlab("Epsilon Threshold") + 
+  ylab("TP/TN/FP/FN (%)") + 
+  facet_grid(test ~ phenotype_phylogenetic_signal)
+ggsave("../figures/tp_tn_fp_fn_vs_epsilon_line_plot.pdf", height = 6, width = 8.5, units = "in")
+

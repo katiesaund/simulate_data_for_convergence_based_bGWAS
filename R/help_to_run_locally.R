e108faad4510b26eb0cf2e562f703276bf09b838
Katie Saund <katiephd@umich.edu>
2020-03-12 16:58:15 -0400

    Temp file for running simulate interactively

---
+++ R/help_to_run_locally.R
@@ -0,0 +1,20 @@
+suppressWarnings(library(ape))
+suppressWarnings(library(caper))
+suppressWarnings(library(phytools))
+suppressWarnings(library(scales))
+
+source("R/tree.R")
+source("R/discrete_trait_lib.R")
+source("R/continuous_trait_lib.R")
+source("R/transition_edges.R")
+source("R/gamma.R")
+source("R/save_data.R")
+source("R/keep_interesting_genotypes.R")
+source("R/ancestral_reconstruction.R")
+source("R/high_confidence.R")
+
+# Initialize variables / read in user input
+num_trees <- 2
+num_phenos <- 2
+num_tips <- 30
+num_start_trait <- 50

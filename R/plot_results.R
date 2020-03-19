67c6ff9f3beb1387c0ee3c227210016004519a4c
Katie Saund <katiephd@umich.edu>
2020-03-18 21:03:42 -0400

    Add a plot for for just continuous results and just binary results

--- R/plot_results.R
+++ R/plot_results.R
@@ -223,5 +223,35 @@ df %>%
   ggtitle("P-value vs. Epsilon") +
   facet_grid(phenotype_type + phenotype_phylogenetic_signal + test ~ tree_id + phenotype_id) + 
   geom_hline(aes(yintercept = -log(0.005)), color = "red")
-ggsave("../figures/pval_vs_epsilon_dot_plot.pdf", height = 6, width = 12, units = "in")
+ggsave("../figures/pval_vs_epsilon_dot_plot_bin_and_cont.pdf", height = 6, width = 12, units = "in")
+
+# signficance vs epsilon - Just continuous
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  filter(test == "continuous") %>% 
+  ggplot() +
+  geom_jitter(aes(x = epsilon, y = fdr_corrected_pvals)) + 
+  xlab("Epsilon") + 
+  theme_bw() + 
+  ylab("-log(P-value)") + 
+  ggtitle("P-value vs. Epsilon") +
+  facet_grid(phenotype_type + phenotype_phylogenetic_signal + test ~ tree_id + phenotype_id) + 
+  geom_hline(aes(yintercept = -log(0.05)), color = "red")
+ggsave("../figures/pval_vs_epsilon_dot_plot_only_continuous.pdf", height = 6, width = 12, units = "in")
+
+# signficance vs epsilon - Just continuous
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  filter(test != "continuous") %>% 
+  ggplot() +
+  geom_jitter(aes(x = epsilon, y = fdr_corrected_pvals)) + 
+  xlab("Epsilon") + 
+  theme_bw() + 
+  ylab("-log(P-value)") + 
+  ggtitle("P-value vs. Epsilon") +
+  facet_grid(phenotype_type + phenotype_phylogenetic_signal + test ~ tree_id + phenotype_id) + 
+  geom_hline(aes(yintercept = -log(0.05)), color = "red")
+ggsave("../figures/pval_vs_epsilon_dot_plot_only_binary.pdf", height = 6, width = 12, units = "in")
 

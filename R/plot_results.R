9248a9b3655437ee4f913b90e0061b3036bde02d
Katie Saund <katiephd@umich.edu>
2020-03-19 13:17:26 -0400

    Update alpha. Add new grouped figured

--- R/plot_results.R
+++ R/plot_results.R
@@ -5,6 +5,65 @@ df <- read_tsv(file = "../data/F1_scores_by_genotype_range_of_alpha_gamma_combin
 df$phenotype_id <- paste0("pheno_", df$phenotype_id)
 df$tree_id <- paste0("tree_", df$tree_id)
 
+### FOR PAPER FIGURE 5
+# Figure 5A
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  filter(test != "continuous") %>% 
+  ggplot() +
+  geom_point(aes(x = epsilon, y = fdr_corrected_pvals), size = 0.5, 
+             alpha = 0.1) + 
+  xlab("") + 
+  theme_bw() + 
+  ylab("") + 
+  facet_grid(tree_id + phenotype_id ~ phenotype_phylogenetic_signal + test) + 
+  theme(axis.text.x = element_text(color = "black", size = 10, angle = 90, hjust = .5, vjust = .5),
+        axis.text.y = element_text(color = "black", size = 10, angle = 0)) + 
+  ggsave("../figures/Fig_5A_pval_vs_epsilon_dot_plot_only_binary.pdf", height = 16, width = 7, units = "in")
+
+# Figure 5B
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  filter(test == "continuous") %>% 
+  ggplot() +
+  geom_point(aes(x = epsilon, y = fdr_corrected_pvals), 
+             size = 0.5, 
+             alpha = 0.1) + 
+  xlab("") + 
+  theme_bw() + 
+  ylab("") + 
+  facet_grid(tree_id + phenotype_id ~ phenotype_phylogenetic_signal + test) + 
+  theme(axis.text.x = element_text(color = "black", size = 10, angle = 90, hjust = .5, vjust = .5),
+        axis.text.y = element_text(color = "black", size = 10, angle = 0)) +
+  ggsave("../figures/Fig_5B_pval_vs_epsilon_dot_plot_only_continuous.pdf", height = 16, width = 3.5, units = "in")
+
+
+### END FIGURE 5
+
+
+## overlay by group
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  ggplot() +
+  geom_point(aes(x = epsilon, 
+                 y = fdr_corrected_pvals, 
+                 color = tree_id:phenotype_id), 
+             size = 0.5, 
+             alpha = 0.1) + 
+  xlab("") + 
+  theme_bw() + 
+  ylab("") + 
+  facet_grid(phenotype_phylogenetic_signal ~ test) + 
+  theme(axis.text.x = element_text(color = "black", size = 10, angle = 90, hjust = .5, vjust = .5),
+        axis.text.y = element_text(color = "black", size = 10, angle = 0)) +
+  ggsave("../figures/pval_vs_epsilon_dot_plot_overlay.pdf", height = 8, width = 4, units = "in")
+
+
+##
+
 df %>% 
   ggplot() + 
   geom_histogram(mapping = aes(x = epsilon, fill = epsilon > 0.50)) +
@@ -267,5 +326,46 @@ df %>%
   ylab("-log(P-value)") + 
   ggtitle("P-value vs. Epsilon") +
   facet_grid(phenotype_type + phenotype_phylogenetic_signal + test ~ tree_id + phenotype_id) + 
-  geom_hline(aes(yintercept = -log(0.05)), color = "red")
 ggsave("../figures/pval_vs_epsilon_dot_plot_only_continuous_BM.pdf", height = 6, width = 12, units = "in")
+
+
+# P-value histogram
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  ggplot() +
+  geom_histogram(aes(x = fdr_corrected_pvals)) + 
+  xlab("-log(P-value); red = -log(0.05)") + 
+  theme_bw() + 
+  ylab("Count") + 
+  ggtitle("P-value") +
+  geom_vline(aes(xintercept = -log(0.05)), color = "red") + 
+  facet_grid(tree_id + phenotype_id ~ phenotype_type + phenotype_phylogenetic_signal + test) + 
+  ggsave("../figures/pval_histogram.pdf", height = 6, width = 12, units = "in")
+
+# P-value histogram
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  filter(test == "continuous", phenotype_phylogenetic_signal == "BM") %>% 
+  ggplot() +
+  geom_histogram(aes(x = fdr_corrected_pvals)) + 
+  xlab("-log(P-value)") + 
+  theme_bw() + 
+  ylab("Count") + 
+  ggtitle("P-value") +
+  facet_grid(tree_id + phenotype_id ~ phenotype_type + test + phenotype_phylogenetic_signal) + 
+  ggsave("../figures/pval_histogram_just_continuous_BM.pdf", height = 6, width = 12, units = "in")
+
+df %>% 
+  filter(epsilon_threshold < 0.04, 
+         alpha_threshold == -log(0.05)) %>% 
+  filter(test != "continuous") %>% 
+  ggplot() +
+  geom_histogram(aes(x = fdr_corrected_pvals)) + 
+  xlab("-log(P-value)") + 
+  theme_bw() + 
+  ylab("Count") + 
+  ggtitle("P-value") +
+  facet_grid(tree_id + phenotype_id ~ phenotype_type + test + phenotype_phylogenetic_signal) + 
+  ggsave("../figures/pval_histogram_just_binary.pdf", height = 6, width = 12, units = "in")
